d1060c0c8a8e649b000d43378afebe7f00f4430c
ivan-commits <ivancolonneau@email.com>
2020-05-04 22:08:48 +0200

    correction bug reaval competence

--- app/scroll.js
+++ app/scroll.js
@@ -21,7 +21,7 @@ document.querySelectorAll('.reveal').forEach(function(r){
 const handleIntersect1 = function (entries, observer){
     entries.forEach(function(entry){
         if(entry.intersectionRatio > ratio){
-            console.log('visible')
+            console.log('visible 1')
             entry.target.classList.replace('reveal-1','reveal-visible-1')
             observer.unobserve(entry.target)
         }
@@ -35,7 +35,7 @@ document.querySelectorAll('.reveal-1').forEach(function(r){
 const handleIntersect2 = function (entries, observer){
     entries.forEach(function(entry){
         if(entry.intersectionRatio > ratio){
-            console.log('visible')
+            console.log('visible 2')
             entry.target.classList.replace('reveal-2','reveal-visible-2')
             observer.unobserve(entry.target)
         }
@@ -43,5 +43,19 @@ const handleIntersect2 = function (entries, observer){
 }
 var observer = new IntersectionObserver(handleIntersect2, options);
 document.querySelectorAll('.reveal-2').forEach(function(r){
+    observer.observe(r)
+});
+
+const handleIntersect3 = function (entries, observer){
+    entries.forEach(function(entry){
+        if(entry.intersectionRatio > ratio){
+            console.log('visible 3')
+            entry.target.classList.replace('reveal-3','reveal-visible-3')
+            observer.unobserve(entry.target)
+        }
+    })
+}
+var observer = new IntersectionObserver(handleIntersect3, options);
+document.querySelectorAll('.reveal-3').forEach(function(r){
     observer.observe(r)
 });
