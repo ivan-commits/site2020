4ada3f78c321a6dc77fb261c164f0372e1c4591d
ivan-commits <ivancolonneau@email.com>
2020-05-17 12:35:20 +0200

    correction htmlvalidator

--- app/scroll.js
+++ app/scroll.js
@@ -18,17 +18,3 @@ document.querySelectorAll('.reveal').forEach(function(r){
     observer.observe(r)
 });
 
-
-const handleIntersect2 = function (entries, observer){
-    entries.forEach(function(entry){
-        if(entry.intersectionRatio > ratio){
-           
-            entry.target.classList.replace('reveal-2','reveal-visible-2')
-            observer.unobserve(entry.target)
-        }
-    })
-}
-var observer = new IntersectionObserver(handleIntersect2, options);
-document.querySelectorAll('.reveal-2').forEach(function(r){
-    observer.observe(r)
-});
